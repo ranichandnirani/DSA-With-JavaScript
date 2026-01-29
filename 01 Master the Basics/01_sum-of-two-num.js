7d71d428aaf68cc4e210f4870b35fcbf46c261c1
ranichandnirani <chandnirani229@gmail.com>
2026-01-29 16:18:41 +0530

    some basics

---
+++ 01 Master the Basics/01_sum-of-two-num.js
@@ -0,0 +1,11 @@
+// let a = 10; number
+// let b = 20;
+
+// console.log(a + b)
+
+// String or number
+
+let a = 10
+let b = "112"
+
+console.log(typeof(a + b)) //output is 10112 => "string"
---
+++ 01 Master the Basics/02_sum-and-message.js
@@ -0,0 +1,5 @@
+let a = 10;
+let b = 20;
+
+console.log("sum of 10 and 20 " + a + b);//output => sum of 10 and 20 1020 (it's a string) (+) is us for concatinate
+console.log("sum of 10 and 20 is "+ (a + b));//output => sum of 10 and 20 is 30(bcz here shows prioritie according BODMAS rule)
